ffa1e1a6a5bb698851474c401ef344408c41d8c1
akavaliova <kovaleva89alena@gmail.com>
2023-06-19 16:42:45 +0200

    removed the comma after state.cash = action.payload

--- src/srore/reducers.ts
+++ src/srore/reducers.ts
@@ -14,7 +14,7 @@ reducers: {
     // doesn't actually mutate the state because it uses the Immer library,
     // which detects changes to a "draft state" and produces a brand new
     // immutable state based off those changes
-    state.cash = action.payload,
+    state.cash = action.payload
   }}
 })
 
